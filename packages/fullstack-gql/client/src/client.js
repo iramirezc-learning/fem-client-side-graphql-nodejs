93860eb03c38070a849f7a5a4c52a461c8b67951
Isaac Ramirez <iramirezc@live.com.mx>
2021-02-25 01:13:20 -0600

    Apollo React: - create Apollo client - wrap App into an ApolloProvider

--- packages/fullstack-gql/client/src/client.js
+++ packages/fullstack-gql/client/src/client.js
@@ -6,3 +6,30 @@ import gql from "graphql-tag";
 /**
  * Create a new apollo client and export as default
  */
+const uri = "http://localhost:4000"; // "https://rickandmortyapi.com/graphql";
+const link = new HttpLink({ uri });
+const cache = new InMemoryCache();
+const client = new ApolloClient({ link, cache });
+
+// How to test it?
+//
+// const query = gql`
+//   {
+//     characters {
+//       results {
+//         name
+//         gender
+//         type
+//       }
+//     }
+//   }
+// `;
+//
+// client
+//   .query({ query })
+//   .then((result) => {
+//     console.log(result);
+//   })
+//   .catch(console.error);
+
+export default client;
