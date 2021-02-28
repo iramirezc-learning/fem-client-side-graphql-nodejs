f5f3d9cb939e909e6174e7f6d5de4e587298178b
Isaac Ramirez <iramirezc@live.com.mx>
2021-02-28 00:09:19 -0600

    Optimistic UI: - make client to be slower using a new Apollo Link

--- packages/fullstack-gql/client/src/client.js
+++ packages/fullstack-gql/client/src/client.js
@@ -1,13 +1,27 @@
 import { ApolloClient } from "apollo-client";
 import { InMemoryCache } from "apollo-cache-inmemory";
 import { HttpLink } from "apollo-link-http";
+import { ApolloLink } from "apollo-link";
+import { setContext } from "apollo-link-context";
 import gql from "graphql-tag";
 
 /**
  * Create a new apollo client and export as default
  */
 const uri = "http://localhost:4000"; // "https://rickandmortyapi.com/graphql";
-const link = new HttpLink({ uri });
+const httpLink = new HttpLink({ uri });
+
+// START: logic to make te server slower
+const delay = setContext((request) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve();
+    }, 1500);
+  });
+});
+const link = ApolloLink.from([delay, httpLink]);
+// END: logic to make te server slower
+
 const cache = new InMemoryCache();
 const client = new ApolloClient({ link, cache });
 
